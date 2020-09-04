a8a3e595175d7a16ca115a855d66249d559a6465
Raphael Lehmann <raphael@rleh.de>
2020-09-04 21:52:45 +0200

    Build in /opt instead of /work

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 SRC="src"
 BUILD="build"
-INSTALL="avr-gcc" # tar.bz2 needs a prefix of 'avr-gcc'
+INSTALL="/opt/avr-gcc" # tar.bz2 needs a prefix of 'avr-gcc'
 DOWNLOAD="download"
 mkdir ${SRC}
 mkdir ${BUILD}
@@ -27,19 +27,19 @@ cd ${SRC}/binutils-${VERSION_BINUTILS}
 patch -g 0 -f -p0 -i ../../avr-binutils-${VERSION_BINUTILS}-size.patch
 mkdir build && cd build
 # configure and make
-../configure --prefix=${root}/${INSTALL}/avr-binutils/ --target=avr --disable-nls --disable-werror
+../configure --prefix=${INSTALL}/avr-binutils/ --target=avr --disable-nls --disable-werror
 make -j${cores}
 make install
 
-# prepend path of newly compiled avr-gcc
-export PATH=${root}/${INSTALL}/avr-binutils/bin:$PATH
+# prepend path of newly compiled avr-binutils
+export PATH=${INSTALL}/avr-binutils/bin:$PATH
 
 cd ${root}
 cd ${SRC}/gcc-${VERSION_GCC}
 mkdir build && cd build
-../configure --target=avr --prefix=${root}/${INSTALL}/avr-gcc/ \
-        --with-ld=${root}/${INSTALL}/avr-binutils/bin/avr-ld \
-        --with-as=${root}/${INSTALL}/avr-binutils/bin/avr-as \
+../configure --target=avr --prefix=${INSTALL}/avr-gcc/ \
+        --with-ld=${INSTALL}/avr-binutils/bin/avr-ld \
+        --with-as=${INSTALL}/avr-binutils/bin/avr-as \
         --enable-languages=c,c++ --with-dwarf2 \
         --disable-nls --disable-libssp --disable-shared \
         --disable-threads --disable-libgomp --disable-bootstrap
@@ -47,13 +47,13 @@ make -j${cores}
 make install
 
 # prepend path of newly compiled avr-gcc
-export PATH=${root}/${INSTALL}/avr-gcc/bin:$PATH
+export PATH=${INSTALL}/avr-gcc/bin:$PATH
 
 cd ${root}
 cd ${SRC}/avr-libc-${VERSION_LIBC}
 patch -g 0 -f -p0 -i ../../avr-libc-${VERSION_LIBC}-atmega168pb.patch
 build=`./config.guess`
-./configure --build=${build} --prefix=${root}/${INSTALL}/avr-gcc --host=avr
+./configure --build=${build} --prefix=${INSTALL}/avr-gcc --host=avr
 make install -j${cores}
 
 cd ${root}
