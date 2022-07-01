6280aeb88c8b383f5ff1f10c40fbede66d9a2540
Raphael Lehmann <raphael@rleh.de>
2022-07-01 14:54:26 +0200

    Update for GCC 12.1 on Ubuntu 22.04

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,11 @@
-FROM ubuntu:20.04
-LABEL maintainer="Niklas Hauser <niklas.hauser@rwth-aachen.de>"
+FROM ubuntu:22.04
+LABEL maintainer="Niklas Hauser <niklas.hauser@rwth-aachen.de>, Raphael Lehmann <raphael+docker@rleh.de>"
 LABEL Description="Image for building the upstream AVR GNU toolchain for Linux"
 WORKDIR /work
 
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update \
     && apt-get upgrade -y \
-    && apt-get install -y \
-        git \
-        wget \
-        autoconf2.64 \
-        build-essential \
-        netpbm \
-        libmpc-dev \
-        libmpfr-dev \
-        libgmp-dev \
-        libmpfr6 \
-        texinfo \
-        doxygen \
-        flex \
-        bison \
-        libexpat1-dev \
-        slang-xfig \
+    && apt-get install -y git wget autoconf2.64 build-essential netpbm libmpc-dev libmpfr-dev libgmp-dev libmpfr6 texinfo doxygen flex bison libexpat1-dev slang-xfig \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
--- build.sh
+++ build.sh
@@ -9,23 +9,23 @@ mkdir ${BUILD}
 mkdir ${INSTALL}
 root=$(pwd)
 cores=8
-#VERSION_BINUTILS="2.37"
-VERSION_GCC="11.2.0"
-VERSION_LIBC="2.0.0"
+#VERSION_BINUTILS="2.38"
+VERSION_GCC="12.1.0"
+VERSION_LIBC="2.1.0"
 
 # Get sources
-wget -q   "https://raw.githubusercontent.com/archlinux/svntogit-community/c3efadcb76f4d8b1a3784015e7c472f59dbfa7de/avr-binutils/repos/community-x86_64/avr-size.patch" &
-wget -q   "https://raw.githubusercontent.com/osx-cross/homebrew-avr/d2e2566b06b90355952ed996707a0a1a24673cd3/Patch/avr-libc-add-mcu-atmega168pb.patch" &
+##wget -q   "https://raw.githubusercontent.com/archlinux/svntogit-community/c3efadcb76f4d8b1a3784015e7c472f59dbfa7de/avr-binutils/repos/community-x86_64/avr-size.patch" &
+##wget -q   "https://raw.githubusercontent.com/osx-cross/homebrew-avr/d2e2566b06b90355952ed996707a0a1a24673cd3/Patch/avr-libc-add-mcu-atmega168pb.patch" &
 #wget -q   "https://raw.githubusercontent.com/osx-cross/homebrew-avr/18d50ba2a168a3b90a25c96e4bc4c053df77d7dc/Patch/avr-binutils-elf-bfd-gdb-fix.patch" &
-wget -qO- "https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.bz2" | tar xj --directory ${SRC} &
+wget -qO- "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.bz2" | tar xj --directory ${SRC} &
 wget -qO- "https://ftp.gnu.org/gnu/gcc/gcc-${VERSION_GCC}/gcc-${VERSION_GCC}.tar.xz" | tar xJ --directory ${SRC} &
 wget -qO- "https://download.savannah.gnu.org/releases/avr-libc/avr-libc-${VERSION_LIBC}.tar.bz2" | tar xj --directory ${SRC} &
 wait
 
 # Build binutils first
-cd ${SRC}/binutils-2.37
+cd ${SRC}/binutils-2.38
 # patch size file
-patch -g 0 -f -p0 -i ../../avr-size.patch
+##patch -g 0 -f -p0 -i ../../avr-size.patch
 mkdir build && cd build
 # configure and make
 ../configure \
@@ -66,7 +66,7 @@ make install
 
 cd ${root}
 cd ${SRC}/avr-libc-${VERSION_LIBC}
-patch -g 0 -f -p0 -i ../../avr-libc-add-mcu-atmega168pb.patch
+##patch -g 0 -f -p0 -i ../../avr-libc-add-mcu-atmega168pb.patch
 build=`./config.guess`
 ./configure \
         --build=${build} \
@@ -76,5 +76,5 @@ make install -j${cores}
 
 cd ${root}
 rm -r build src
-rm avr-size.patch
-rm avr-libc-add-mcu-atmega168pb.patch
+##rm avr-size.patch
+##rm avr-libc-add-mcu-atmega168pb.patch
